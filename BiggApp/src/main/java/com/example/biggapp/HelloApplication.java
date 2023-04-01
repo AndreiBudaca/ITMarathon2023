f3a7c24dc6609db6d1d417359fad6d85c642c1ce
danelthedev <danelthedev@gmail.com>
2023-04-01 13:25:13 +0100

    completed login page

--- BiggApp/src/main/java/com/example/biggapp/HelloApplication.java
+++ BiggApp/src/main/java/com/example/biggapp/HelloApplication.java
@@ -11,15 +11,16 @@ import com.example.biggapp.Request.APICaller;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoginPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        stage.setTitle("Break Inviter");
         stage.setScene(scene);
         stage.show();
+        stage.setResizable(false);
     }
 
     public static void main(String[] args) {
-        //launch();
+        launch();
         APICaller caller = new APICaller();
 
         try {
--- BiggApp/src/main/java/com/example/biggapp/HelloController.java
+++ BiggApp/src/main/java/com/example/biggapp/HelloController.java
@@ -2,12 +2,30 @@ package com.example.biggapp;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+
     @FXML
-    private Label welcomeText;
+    private Label LoginErrorLabel;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField LoginUsernameTextField;
+    @FXML
+    private TextField LoginPasswordTextField;
+
+    @FXML
+    protected void onLoginClick() {
+        String username = LoginUsernameTextField.getText();
+        String password = LoginPasswordTextField.getText();
+
+        //TODO: send request to API and:
+
+        //TODO: Redirect if good
+        //...
+
+        //Display error if bad
+        {
+            LoginErrorLabel.setVisible(true);
+        }
     }
 }
