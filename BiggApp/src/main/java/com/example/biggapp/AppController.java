221b97b1ef082a1b7434c4ac3ed7318325583e9c
danelthedev <danelthedev@gmail.com>
2023-04-01 16:13:02 +0100

    created popup

--- BiggApp/src/main/java/com/example/biggapp/AppController.java
+++ BiggApp/src/main/java/com/example/biggapp/AppController.java
@@ -3,20 +3,21 @@ package com.example.biggapp;
 import com.example.biggapp.Request.APICaller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class AppController {
+public class AppController{
     //login page
     @FXML
     private Button LoginButton;
@@ -118,5 +119,59 @@ public class AppController {
     }
 
     //send request page
+    @FXML
+    private Button SendRequestBackButton;
+    @FXML
+    private Button SendRequestSendButton;
+    @FXML
+    private TextArea InvitedPersonsTextArea;
+    @FXML
+    private Button AddPersonButton;
+    @FXML
+    private TextField LocationTextField;
+    @FXML
+    private TextArea CommentTextArea;
+
+    @FXML
+    protected void onAddPersonClick(){
+
+    }
+
+    @FXML
+    protected void onSendRequestSendClick(){
+
+    }
+
+    @FXML
+    protected void onSendRequestBackClick() throws IOException {
+        //Create pop-up
+        final Stage dialog = new Stage();
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.initOwner((Stage) SendRequestBackButton.getScene().getWindow());
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("SendRequestBackPopUp.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 450, 150);
+        dialog.setScene(scene);
+        dialog.show();
+    }
+
+    //send request back pop-up
+    @FXML
+    private Button SendRequestBackCancelButton;
+    @FXML
+    private Button SendRequestBackContinueButton;
+    @FXML
+    protected void onSendRequestBackCancelClick(){
+        Stage popup = (Stage) SendRequestBackCancelButton.getScene().getWindow();
+        popup.close();
+    }
+    @FXML
+    protected void onSendRequestBackContinueClick() throws IOException {
+        Stage popup = (Stage) SendRequestBackCancelButton.getScene().getWindow();
+        popup.close();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("HomePage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+
+    }
 
 }
