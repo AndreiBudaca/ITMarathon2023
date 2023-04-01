084d477644c7b24845e3920e8a9505494baa9319
danelthedev <danelthedev@gmail.com>
2023-04-01 20:02:51 +0100

    stuff

--- BiggApp/src/main/java/com/example/biggapp/Main.java
+++ BiggApp/src/main/java/com/example/biggapp/Main.java
@@ -1,5 +1,6 @@
 package com.example.biggapp;
 
+import com.example.biggapp.Request.APICaller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,15 +8,55 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import com.example.biggapp.ServerCommunication.*;
 
 public class Main extends Application {
     public static Stage primaryStage;
+    public static String credentials = "credentials.txt";
 
     @Override
     public void start(Stage stage) throws IOException {
 
+        String username = "";
+
+        try {
+            String creds = Files.readString(Paths.get(credentials), StandardCharsets.US_ASCII);
+            username = creds.split(" ")[0];
+        }
+        catch (Exception e){
+            username = "";
+        }
+
+        APICaller apiCaller = new APICaller();
+        boolean succes = false;
+
+        try{
+            succes = apiCaller.checkActiveSessionStatus(username);
+        }
+        catch (Exception ignore){
+
+        }
+
+        if (succes){
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("HomePage.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+            stage.setTitle("Break Inviter");
+            stage.setScene(scene);
+            stage.show();
+            primaryStage = stage;
+        }
+        else{
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LoginPage.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+            stage.setTitle("Break Inviter");
+            stage.setScene(scene);
+            stage.show();
+            primaryStage = stage;
+        }
 
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LoginPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
@@ -33,7 +74,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-       launch();
+        launch();
         try {
             ClientSocket client = new ClientSocket();
             client.test();
